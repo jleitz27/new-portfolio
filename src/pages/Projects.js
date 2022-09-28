b580b67309fa91dc95a6e5c95a567e102fb4e30a
Jason L <jkleitz27@gmail.com>
2022-09-28 18:09:32 -0400

    updated project order

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -13,11 +13,11 @@ import bookden from '../assets/images/bookden.png'
 
 const projectData = [
   {
-      title: 'Beer Me',
-      repoName: 'beer-me',
-      deployedName: 'https://fpierrerun.github.io/BeerMe/',
-      image: beer,
-      description: "Provides data on local breweries status based on searching for a city. Brewery data comes from the Open Brewery API. Location and map data comes from Google Maps API. We also used Bulma for styling."
+    title: 'Book Den',
+    repoName: 'book-den',
+    deployedName: 'https://glacial-ridge-11514.herokuapp.com/',
+    image: bookden,
+    description: "This is a social media site that also uses the Google Books API search engine. The app was based on the MERN stack model with a React front end, MongoDB database, and Node.js/Express.js server and API. In addition to being able to search for books, if signed in, the user can save the books that catch their eye. Later, the user can delete those books once they've been read."
   },
   {
       title: 'Tech Blog',
@@ -48,11 +48,11 @@ const projectData = [
       description: "This is a budget tracking application that allows for offline access and functionality.The user can add expenses and deposits to their budget with or without a connection. When entering transactions offline, they should populate the total when brought back online. This is an exercise in implementing offline functionality. It uses the browser's indexDB while offline, and MongoDB while online."
   },
   {
-      title: 'Book Den',
-      repoName: 'book-den',
-      deployedName: 'https://glacial-ridge-11514.herokuapp.com/',
-      image: bookden,
-      description: "This is a social media site that also uses the Google Books API search engine. The app was based on the MERN stack model with a React front end, MongoDB database, and Node.js/Express.js server and API. In addition to being able to search for books, if signed in, the user can save the books that catch their eye. Later, the user can delete those books once they've been read."
+    title: 'Beer Me',
+    repoName: 'beer-me',
+    deployedName: 'https://fpierrerun.github.io/BeerMe/',
+    image: beer,
+    description: "Provides data on local breweries status based on searching for a city. Brewery data comes from the Open Brewery API. Location and map data comes from Google Maps API. We also used Bulma for styling."
   },
 ];
 
